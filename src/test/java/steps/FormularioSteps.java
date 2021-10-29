71838740ca2ccdf9453990daa42f66a32286bc2d
Renan Rocha <renanprch@gmail.com>
2021-10-29 18:10:56 -0300

    Alguns ajustes

--- src/test/java/steps/FormularioSteps.java
+++ src/test/java/steps/FormularioSteps.java
@@ -1,19 +1,46 @@
 package steps;
 
+import org.junit.After;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.Before;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.E;
+import io.cucumber.java.it.Quando;
+import io.cucumber.java.pt.Entao;
 import support.AbrirNavegador;
 
 public class FormularioSteps extends AbrirNavegador {
+	
+	WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = AbrirNavegador.abrirNavegadorChrome();
+	}
+	
 	@Dado("que estou no menu Automobile Insurance do site SampleApp")
 	public void que_estou_no_menu_automobile_insurance_do_site_sample_app() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
 	}
 
 	@E("preencho os campos da aba {string}")
 	public void preencho_os_campos_da_aba(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+
+	}
+	
+	@Quando("clico no bot�o Send")
+	public void clico_no_bot_o_send() {
+	
+	}
+	
+	@Entao("devo ver a mensagem {string} na tela")
+	public void devo_ver_a_mensagem_na_tela(String string) {
+	
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
 	}
 }
